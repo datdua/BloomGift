738639d12c6e9a1ba6332cccabc631747f65d2e7
trinh <trinhbloc2003@gmail.com>
2024-09-11 22:34:32 +0700

    CRUD product 3

--- Back-End/bloomgift/bloomgift/src/main/java/com/example/bloomgift/repository/ProductRepository.java
+++ Back-End/bloomgift/bloomgift/src/main/java/com/example/bloomgift/repository/ProductRepository.java
@@ -1,15 +1,24 @@
 package com.example.bloomgift.repository;
 
-import java.util.List;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
 import com.example.bloomgift.model.Product;
+import com.example.bloomgift.model.Promotion;
+import com.example.bloomgift.model.Store;
 
-public interface ProductRepository extends JpaRepository<Product,Integer> {
 
+public interface ProductRepository extends JpaRepository<Product,Integer> , JpaSpecificationExecutor<Product>{
 
+    List<Product> findProductByStoreID(Store storeID);
+    
+    // @Query("SELECT p FROM Product p WHERE p.storeID = :storeID, p.featured = 'true'")
+    // List<Product> findProductByStoreIDandFeatured(@Param("storeID") int storeID);
   
 }
