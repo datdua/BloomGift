28f06683b655526e1131ca958475e6a0d1359403
trinh <trinhbloc2003@gmail.com>
2024-09-14 02:26:33 +0700

    CRUD cart 1

--- Back-End/bloomgift/bloomgift/src/main/java/com/example/bloomgift/config/FirebaseConfig.java
+++ Back-End/bloomgift/bloomgift/src/main/java/com/example/bloomgift/config/FirebaseConfig.java
@@ -1,30 +1,30 @@
-package com.example.bloomgift.config;
+// package com.example.bloomgift.config;
 
-import com.example.bloomgift.BloomgiftApplication;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+// import com.example.bloomgift.BloomgiftApplication;
+// import com.google.auth.oauth2.GoogleCredentials;
+// import com.google.firebase.FirebaseApp;
+// import com.google.firebase.FirebaseOptions;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.core.io.ClassPathResource;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+// import java.io.File;
+// import java.io.FileInputStream;
+// import java.io.IOException;
 
-@Configuration
-public class FirebaseConfig {
-    private static final String SERCURITY_FIREBASE = "serviceAccountKey.json";
+// @Configuration
+// public class FirebaseConfig {
+//     private static final String SERCURITY_FIREBASE = "serviceAccountKey.json";
 
-    @SuppressWarnings("deprecation")
-    @Bean
-    public FirebaseApp firebaseApp() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream(new ClassPathResource(SERCURITY_FIREBASE).getFile());
+//     @SuppressWarnings("deprecation")
+//     @Bean
+//     public FirebaseApp firebaseApp() throws IOException {
+//         FileInputStream serviceAccount = new FileInputStream(new ClassPathResource(SERCURITY_FIREBASE).getFile());
         
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build();
+//         FirebaseOptions options = new FirebaseOptions.Builder()
+//                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//                 .build();
 
-        return FirebaseApp.initializeApp(options);
-    }
-}
+//         return FirebaseApp.initializeApp(options);
+//     }
+// }
