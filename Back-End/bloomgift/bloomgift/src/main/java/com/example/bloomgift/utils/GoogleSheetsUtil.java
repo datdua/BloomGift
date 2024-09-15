673aba965abf2ce9d6ab250ace7bee2711a52722
ThDosu <phamlethanhdung04@gmail.com>
2024-09-15 22:16:11 +0700

    update ggSheet

--- Back-End/bloomgift/bloomgift/src/main/java/com/example/bloomgift/utils/GoogleSheetsUtil.java
+++ Back-End/bloomgift/bloomgift/src/main/java/com/example/bloomgift/utils/GoogleSheetsUtil.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
@@ -20,7 +20,6 @@ import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
-import com.google.api.services.gmail.GmailScopes;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
 
@@ -30,8 +29,8 @@ public class GoogleSheetsUtil {
 
     private static final String APPLICATION_NAME = "Google Sheets API Java Quickstart";
     private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-    private static final String TOKENS_DIRECTORY_PATH = "tokens";
-    private static final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS);
+    private static final String TOKENS_DIRECTORY_PATH = "tokens/path";
+    private static final List<String> SCOPES =  Collections.singletonList(SheetsScopes.SPREADSHEETS);
     private static final String CREDENTIALS_FILE_PATH = "credentials.json";
 
     public Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
