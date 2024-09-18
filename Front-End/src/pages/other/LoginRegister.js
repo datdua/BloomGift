7dcb24d370f496840e34126fcc14bcab711d0b47
Nguyễn Tấn Đạt <toimqn09@gmail.com>
2024-09-18 17:15:05 +0700

    change-title-1

--- Front-End/src/pages/other/LoginRegister.js
+++ Front-End/src/pages/other/LoginRegister.js
@@ -220,7 +220,7 @@ const LoginRegister = ({ location }) => {
                                     onClick={() => window.location.href = 'http://localhost:8080/oauth2/authorization/google'}
                                     className="ml-5"
                                   >
-                                    Đăng nhập với Google
+                                    Đăng nhập Google
                                   </button>
                                 </div>
                               </form>
